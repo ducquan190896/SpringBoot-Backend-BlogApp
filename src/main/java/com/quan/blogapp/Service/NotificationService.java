b786890cf8f3a5ce9b443ebff02ff32f06424362
quan doan <ducquan190896@gmail.com>
2022-12-30 13:34:25 +0200

    final commit

--- src/main/java/com/quan/blogapp/Service/NotificationService.java
+++ src/main/java/com/quan/blogapp/Service/NotificationService.java
@@ -1,5 +1,7 @@
 package com.quan.blogapp.Service;
 
+import java.util.List;
+
 import com.quan.blogapp.Entity.Comment;
 import com.quan.blogapp.Entity.Notification;
 import com.quan.blogapp.Entity.NotificationType;
@@ -9,4 +11,9 @@ import com.quan.blogapp.Entity.Users;
 public interface NotificationService {
     Notification sendNotification(NotificationType type, Users sender, Users receiver, Post postNotify);
     Notification sendNotification(NotificationType type, Users sender, Users receiver, Comment comment);
+    List<Notification> getNotifications();
+    List<Notification> getNotificationBySender(Long senderId);
+    List<Notification> getNotificationByReceiver(Long receiverId);
+    
+
 }
---
+++ src/main/java/com/quan/blogapp/Service/TagService.java
@@ -0,0 +1,9 @@
+package com.quan.blogapp.Service;
+
+import java.util.List;
+
+import com.quan.blogapp.Entity.Tag;
+
+public interface TagService {
+    List<Tag> getTags();
+}
